22f0bc6d8aa8ef05e0694fbe6197070d82dd85e2
isabellacarrillo <isabella.carrillo@correo.unimet.edu.ve>
2024-02-08 19:47:11 -0400

    company fixed

--- ProyectoOperativos/src/Clases/Company.java
+++ ProyectoOperativos/src/Clases/Company.java
@@ -6,6 +6,7 @@ package Clases;
 
 import java.util.concurrent.Semaphore;
 import Interfaces.DashBoard;
+import Clases.ProjectManager;
 /**
  *
  * @author Admin
@@ -42,8 +43,7 @@ public class Company {
     }
     
     public void initWorkers(){
-        switch(this.name){
-            case "Nickelodeon":
+      
                 for(int i = 0; i<this.guiones; i++){
                     Worker worker = new Worker(480, "guionistas", 0.34f, this.dia, this);
                     worker.start();
@@ -79,51 +79,17 @@ public class Company {
                     worker.start();
                     workers[i+this.guiones+this.escenarios+this.animadores+this.doblajes+this.PlotTwists] = worker;
                 }
-                break;
                 
-            case "Star Channel":
-                 for(int i = 0; i<this.guiones; i++){
-                    Worker worker = new Worker(480, "guionistas", 0.34f, this.dia, this);
-                    worker.start();
-                    workers[i] = worker;
-                }
+            System.out.println("Workers creados "+this.name);
+            ProjectManager pm = new ProjectManager(960, this.dia, this, this.gui);
+            pm.start();
+            Director director = new Director(1440, this.dia, this, this.gui);
+            director.start();
+        }        
 
-                for(int i = 0; i<this.escenarios; i++){
-                    Worker worker = new Worker(624, "escenarios", 0.34f, this.dia, this);
-                    worker.start();
-                    workers[i+this.guiones] = worker;
-                }
-
-                for(int i = 0; i<this.animadores; i++){
-                    Worker worker = new Worker(960, "animadores", 2f, this.dia, this);
-                    worker.start();
-                    workers[i+this.guiones+this.escenarios] = worker;
-                }
-
-                for(int i = 0; i<this.doblajes; i++){
-                    Worker worker = new Worker(384, "doblajes", 5f, this.dia, this);
-                    worker.start();
-                    workers[i+this.guiones+this.escenarios+this.animadores] = worker;
-                }
-
-                for(int i = 0; i<this.PlotTwists; i++){
-                    Worker worker = new Worker(816, "PlotTwist", 0.5f, this.dia, this);
-                    worker.start();
-                    workers[i+this.guiones+this.escenarios+this.animadores+this.doblajes] = worker;
-                }
-
-                for(int i = 0; i<this.ensambladores; i++){
-                    Worker worker = new Worker(1200, "ensamblador", 0.5f, this.dia, this);
-                    worker.start();
-                    workers[i+this.guiones+this.escenarios+this.animadores+this.doblajes+this.PlotTwists] = worker;
-                }
-                break;
-               
-        }
-        System.out.println("Devs creados "+this.name);
     }
 
-}
+
 
 
 
